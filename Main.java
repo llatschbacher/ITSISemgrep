5476ab10958d696f8a94b1556d35925b4c541393
Jakob Ertl <jertl2@student.tgm.ac.at>
2024-03-21 14:42:55 +0100

    Regeln hinzugefuegt

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Rechtschreibtrainer trainer = new Rechtschreibtrainer();
         trainer.runTraining();
+        System.exit();
     }
 }
